9a1a643de6ec4a925d20fc8c8f5892490fb8fb73
tainamartinsz <tainadesouzamartins@gmail.com>
2026-06-05 21:13:04 -0300

    Backend - 05/06

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // import export
 
 import express from "express";
-import routes from "./routes.js";
+import routes from "./src/routes/routes.js";
 
 const app = express();
-app.use('/assets', express.static('assets'));
+app.use(express.json()); 
+app.use(express.static('assets'));
+app.use(express.static('src'));
 app.use(routes);
 
-app.listen('3344', () => { console.log('Servidor ON - Porta 3344'); });
+app.listen(3344, () => { console.log('Servidor ON - Porta 3344'); });
