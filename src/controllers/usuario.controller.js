c64be05bc2c97540ad781070b80ae1771843cb0a
MarcosMendes642 <mendesmarcos403@gmail.com>
2026-06-17 18:59:22 -0300

    reset: remove tudo e adiciona novo conteúdo

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,8 +1,9 @@
 const bcrypt = require("bcrypt");
 const prisma = require("../libs/prisma.js");
+const jwt = require("jsonwebtoken");
 
 async function criarUsuarios(req, res) {
-  try {
+   try {
     const { name, email, cpf, password } = req.body;
     const hashpassword = await bcrypt.hash(password, 10);
 
@@ -90,4 +91,39 @@ async function procurarClirGeral(req, res) {
     });
   }
 }
-module.exports = { criarUsuarios, procurarCliepeloid, procurarClirGeral };
+async function loginUsuario (req, res) {
+  const {email, cpf, password} = req.body;
+
+  if (!password || (!email && !cpf)){
+    return res.status(400).json({ 
+    erro: "Para fazer o login, você deve fornecer a senha e ao menos o e-mail ou o CPF." 
+  });
+  } 
+  const condicoes = [];
+  if (email) condicoes.push({ user_email: email });
+  if (cpf) condicoes.push({ user_cpf: cpf });
+
+  const userExistente= await prisma.usuario.findFirst({
+    where: {
+        OR: condicoes
+    }
+  }) 
+  if(!userExistente){
+    return res.status(401).json({erro: "Não conseguimos encontrar sua conta com esses dados. Verifique as informações e tente de novo."});
+  }
+  const senhaValida = await bcrypt.compare(password, userExistente.user_senha);
+
+  if(!senhaValida){
+    return res.status(401).json({erro: "Não conseguimos encontrar sua conta com esses dados. Verifique as informações e tente de novo."});
+  }
+  const token = jwt.sign(
+    {
+      id: userExistente.user_id.toString(),
+      email: userExistente.user_email
+    },
+     process.env.JWT_SECRET,
+     {expiresIn: '7d'}
+  );
+  return res.status(200).json({ token });
+}
+module.exports = { criarUsuarios, procurarCliepeloid, procurarClirGeral, loginUsuario };
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const autenticar = require('../middlewares/autenticar');
 
-const { criarUsuarios, procurarCliepeloid, procurarClirGeral } = require("../controllers/usuario.controller.js");
+const { criarUsuarios, procurarCliepeloid, procurarClirGeral, loginUsuario } = require("../controllers/usuario.controller.js");
 
-router.post("/usuariosCad", criarUsuarios);
+//Post
+router.post("/Cadastro", criarUsuarios);
+router.post("/Login", loginUsuario);
+
+//Get
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 router.get("/usuario/:user_id", procurarCliepeloid);
 router.get("/usuarioid", procurarClirGeral);
+router.get("/perfil", autenticar, (req, res)=>{
+  res.json({ usuario: req.usuario });
+})
 module.exports = router;
