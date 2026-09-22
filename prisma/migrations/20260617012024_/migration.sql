1e7151729e1ee13f107dfdc5f9ccf53a68598426
EnzoVimercati <enzovimercati@gmail.com>
2026-09-21 23:24:56 -0300

    Create 'usuarios' table with constraints and indexes
    Added table 'usuarios' with primary key and unique indexes.

--- prisma/migrations/20260617012024_/migration.sql
+++ prisma/migrations/20260617012024_/migration.sql
@@ -1,3 +1,21 @@
+-- CreateTable
+CREATE TABLE "usuarios" (
+    "user_id" BIGSERIAL NOT NULL,
+    "user_nome" VARCHAR(40) NOT NULL,
+    "user_email" VARCHAR(40) NOT NULL,
+    "user_senha" VARCHAR(255),
+    "user_cpf" VARCHAR(14) NOT NULL,
+
+    CONSTRAINT "usuarios_pkey" PRIMARY KEY ("user_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuarios_user_email_key" ON "usuarios"("user_email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "usuarios_user_cpf_key" ON "usuarios"("user_cpf");
+
+
 CREATE TABLE IF NOT EXISTS "stream" (
   "cat_id" INTEGER,
   "gru_id" INTEGER NOT NULL UNIQUE,
